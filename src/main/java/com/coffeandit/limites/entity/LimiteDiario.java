ab026f4e43924c028ff8440b683ddad9cb8fd1e8
Paulo Roberto Menezes <paulomenezes.web@gmail.com>
2022-09-26 16:31:13 -0300

    Camada de servico

--- src/main/java/com/coffeandit/limites/entity/LimiteDiario.java
+++ src/main/java/com/coffeandit/limites/entity/LimiteDiario.java
@@ -1,20 +1,30 @@
 package com.coffeandit.limites.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
 @Data
 @EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LimiteDiario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long agencia;
--- src/main/java/com/coffeandit/limites/service/LimiteDiarioService.java
+++ src/main/java/com/coffeandit/limites/service/LimiteDiarioService.java
@@ -3,8 +3,11 @@ package com.coffeandit.limites.service;
 import com.coffeandit.limites.entity.LimiteDiario;
 import com.coffeandit.limites.repository.LimiteDiarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -13,7 +16,24 @@ public class LimiteDiarioService {
 
     private final LimiteDiarioRepository limiteDiarioRepository;
 
+    @Value("${limite.valor:0}")
+    private BigDecimal valorDiario;
+
     public Optional<LimiteDiario> findById(Long id) {
         return limiteDiarioRepository.findById(id);
+    }
+
+    public Optional<LimiteDiario> buscarLimiteDiario(final Long agencia, final Long conta) {
+        Optional<LimiteDiario> limiteDiario = limiteDiarioRepository.findByAgenciaAndConta(agencia, conta);
+        if (limiteDiario.isEmpty()) {
+            LimiteDiario limite = LimiteDiario.builder()
+                    .conta(conta)
+                    .agencia(agencia)
+                    .valor(valorDiario)
+                    .build();
+            return Optional.of(limiteDiarioRepository.save(limite));
+        }
+
+        return limiteDiario;
     }
 }
